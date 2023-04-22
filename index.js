c38d4e3e3c04f69ad71cf45ebe176b5f3edf67ba
José Compadre Junior <jose.compadre@oatsolutions.com.br>
2023-04-22 20:19:15 -0300

    Added macro renderer

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 import '@logseq/libs';
 
-//Insert a button to the block
-async function insertButton(e) {
-  const currentBlock = logseq.Editor.getBlock(e.uuid);
-  const blockWithButton = currentBlock + ' [Button here] ';
-  logseq.Editor.updateBlock(e.uuid, blockWithButton);
-}
-
 const main = async () => {
   console.log('Template Button plugin loaded');
-  logseq.Editor.registerSlashCommand('insertButton', async (e) => {
-    insertButton(e);
+
+  // Register button renderer
+  logseq.Editor.registerSlashCommand('Insert Template Button', async () => {
+    await logseq.Editor.insertAtEditingCursor(`{{renderer :template_button}}`);
+    console.log('Button renderer registered.');
+  });
+
+  logseq.App.onMacroRendererSlotted(({ slot, payload: { arguments } }) => {
+    let [template] = arguments;
+    logseq.provideUI({
+      key: 'template-button',
+      slot,
+      template: `
+      <a>Create Page from Template</a>
+     `,
+    });
   });
 };
 
