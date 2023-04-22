0239984a3da9b4411c7199b89f0cddc815df2797
José Compadre Junior <jose.compadre@oatsolutions.com.br>
2023-04-22 18:30:27 -0300

    Updated package.json git repository URL

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 import '@logseq/libs';
 
-//Inputs 5 numbered blocks when called
-async function insertSomeBlocks(e) {
-  console.log('Open the calendar!');
-  let numberArray = [1, 2, 3, 4, 5];
-  for (const number in numberArray) {
-    logseq.App.showMsg('Function has been run');
-    logseq.Editor.insertBlock(e.uuid, `This is block ${numberArray[number]}`, {
-      sibling: true,
-    });
-  }
+//Insert a button to the block
+async function insertButton(e) {
+  const currentBlock = logseq.Editor.getBlock(e.uuid);
+  const blockWithButton = currentBlock + ' [Button here] ';
+  logseq.Editor.updateBlock(e.uuid, blockWithButton);
 }
 
 const main = async () => {
   console.log('Template Button plugin loaded');
-  logseq.Editor.registerSlashCommand('insertBlocks', async (e) => {
-    insertSomeBlocks(e);
+  logseq.Editor.registerSlashCommand('insertButton', async (e) => {
+    insertButton(e);
   });
 };
 
